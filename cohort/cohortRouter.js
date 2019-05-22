972b4742eb682df8926ea755fcd0b9fdc5e80e15
Sean Doyle <sdoylelambda@gmail.com>
2019-05-22 18:56:50 -0400

    Put and Post routes.

--- cohort/cohortRouter.js
+++ cohort/cohortRouter.js
@@ -23,9 +23,34 @@ const db = knex(config)
     .then(records => res.json(records))
     .catch(err => res.status(500).json(err))
   });
-  
-  router.post
+//?? returns all students for the cohort with the specified id.
+  router.get('/:id.students', (req, res) => {
+    const { id } = req.params;
+    db('cohorts').where({ id })
+    .then(records => res.json(records))
+    .catch(err => res.status(500).json(err))
+  });
 
-  router.delete
+  router.post('/', (req, res) => {
+    const role = req.body;
+    if(role.name && role.name.length) {
+      db('cohorts').insert(roles)
+      .then(name => res.status(201).json(name))
+      .catch(err => res.status(500).json(err))
+    } else {
+      res.status(400).json({error: 'Error'})
+    }
+  });
+
+  router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const role = req.body;
+    db('cohorts').where({ id }).update(role)
+    .then(count => res.json({ count }))
+    .catch(err => req.status(500).json(err))
+  })
+
+
+  router.delete()
 
   module.exports = router;
