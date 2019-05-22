2e3d4331d08da6c550f79e1eaf737679c5efaa84
Sean Doyle <sdoylelambda@gmail.com>
2019-05-22 18:50:14 -0400

    Get routes.

--- cohort/cohortRouter.js
+++ cohort/cohortRouter.js
@@ -2,15 +2,27 @@ const knex = require('knex');
 const router = require('express').Router();
 
 const config = {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/lambda.sqlite3'
-    },
-    debug: true
-  }
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.db3'
+  },
+  debug: true
+}
 
-  router.get
+const db = knex(config)
+ 
+  router.get('/', (req, res) => {
+      db('cohorts').then(records => res.status(201).json(records))
+      .catch(err => res.status(500).json(err))
+  });
+
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db('cohorts').where({ id })
+    .then(records => res.json(records))
+    .catch(err => res.status(500).json(err))
+  });
   
   router.post
 
